ca2527e5beb3452c4c812f78d49019d9d5d8e847
hmalle <hmalle90ak@gmail.com>
2018-06-26 18:39:37 -0700

    solved 60.c but the code might need more work

--- 60.c
+++ 60.c
@@ -1,3 +1,4 @@
+
 /*
   The primes 3, 7, 109, and 673, are quite remarkable. By taking any two primes and concatenating 
   them in any order the result will always be prime. For example, taking 7 and 109, both 7109 and 
@@ -13,96 +14,102 @@
 #include<stdbool.h>
 #include<gmp.h>
 #include<string.h>
+#include<assert.h>
 
-void print_primes(int* primes, int size){
-  printf("[");
-  for(int a=0;a<size;a++){
+void print_primes( int* primes, int size){
+  int sum = 0;
+  printf("%d:[ ", size);
+  for(int a=0; a<size; a++){
+    sum += *(primes+a);
     printf("%d, ", *(primes+a));
   }
-  printf("]\n");
+  printf("] -> sum : %d\n",sum);
 }
 
-int isprime(int i){
+int isprime(char * s){
   mpz_t n;
-  mpz_init_set_ui(n, i);
+  mpz_init_set_str(n, s, 10);
   int status = mpz_probab_prime_p(n, 10);
   mpz_clear(n);
   return status;
 }
 
-int join( int x, int y){
+void join(char* s, int x, int y){
   char a[10]="", b[10] ="";
   sprintf(a,"%d", x);
   sprintf(b,"%d", y);
-  strcat(a, b);
-  //printf("%s\n", a);
-  return atoi(a);
+  strcat(s, a);
+  strcat(s, b);
 }
 
-void print_sum( int* primes, int size){
-  int sum = 0;
-  for(int a=0; a<size; a++){
-    sum += *(primes+a);
-  }
-  printf("sum : %d\n",sum);
-}
-
-bool assign_next_prime(int* primes, int index){
-  //printf("that primes is: %d\n", *(primes+index));
-  for(int a=2+*(primes+index);a<1000000; a+=2){ //TODO: limit that a
-    if(isprime(a)){
-      *(primes+index) = a;
-      return true;
+int get_next_prime(int p){
+  char sp[10]="";
+  int max = 10000;
+  for(int a=2+p; a<max; a+=2){ 
+    if(a>max-2){
+      return 0;
+    }
+    sprintf(sp, "%d", a);
+    if(isprime(sp)){
+      return a;
     }
   }
-  return false;
+  return 0;
 }
 
-int check_primes(int* primes, int size){
-  int n;
+bool check_primes(int* primes, int size){
+  char m[20]="", n[20]="";
   for(int i=0; i<size; i++){
     for(int j=0;j<size; j++){
       if(i==j) continue;
-      n = join( *(primes+i) , *(primes+j) );
-      if( !isprime(n) &&j>i ){
-        return j;
-      }
-      if(!isprime(n)){
-        return j-1;
+      memset(m,0,20);
+      memset(n,0,20);
+      join(m, *(primes+i) , *(primes+j) );
+      join(n, *(primes+j) , *(primes+i) );
+      //printf("%d, %d\n", m, n);
+      if(!isprime(m) || !isprime(n)){
+        return false;
       }
     }
   }
-  return -1;
+  return true;
 }
 
-int main(int argc, char** argv)
-{
-  if(argc!=2){
-    printf("Enter the correct number of arguments\n");
-    exit(0);
-  }
-  int index, size=atoi(argv[1]);
-  bool found = false;
-  int primes[size];
-  //Get the first few primes as needed by the program
-  for(int n=3,p=0; p<size; n+=2){
-    if(isprime(n)){
-      primes[p]= n;
-      p++;
+void check_starting_with(int p){
+  int size=2, max=5, next_prime;
+  int ns[max];
+  ns[0] = p;
+  ns[1] = get_next_prime(p);
+  while(size<=max){
+    if( check_primes( (int *)&ns, size)){
+      if(size > 3){
+        print_primes((int*)&ns, size);
+      }
+      next_prime = get_next_prime(ns[size-1]);
+      ns[size] = next_prime;
+      size++;
+    }else if(next_prime=get_next_prime(ns[size-1]) == 0){
+      if(size>1){
+        next_prime = get_next_prime(ns[size-2]);
+        ns[size-2] = next_prime;
+        size-=1;
+      }
+    } else{
+      next_prime = get_next_prime(ns[size-1]);
+      ns[size-1] = next_prime;
     }
   }
+}
 
-  for(int a=0;!found; a++){
-    index = check_primes( primes, size);
-    printf("checking:");
-    print_primes( (int*)&primes, size);
-    if(index == -1){
-      print_primes((int*)&primes,size);
-      found = true;
-    }else{
-      assign_next_prime((int*)&primes, index);
+int main(int argc, char** argv){
+  char sp[10] = "";
+  //TODO: Make this work through all of them
+  for(int p=3; p<200; p+=2){
+    if(p%2==0 || p%5==0) continue;
+    sprintf(sp, "%d", p);
+    if(isprime(sp)){
+      check_starting_with(p);
     }
   }
-  print_sum( (int*)&primes, size);
   return 0;
 }
