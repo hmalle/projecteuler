c7ba243cfd951f0d448443cc89088be4ac3092a5
hmalle <hmalle90ak@gmail.com>
2018-10-31 05:58:17 -0700

    playing with double arrays

--- 79.c
+++ 79.c
@@ -5,11 +5,13 @@ The text file, keylog.txt, contains fifty successful login attempts.
 
 Given that the three characters are always asked for in order, analyse the file so as to determine the shortest possible secret passcode of unknown length. */
 
+//Hand computed answer: 73162890
+
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
 
-int lines(FILE* fptr){
+int linecount(FILE* fptr){
   //Returns the lines count in the file
   int lines=0;
   char ch;
@@ -22,35 +24,47 @@ int lines(FILE* fptr){
   return lines;
 }
 
-void sort(char **keys, int size){
-
+void sort_unique(char **keys, int size){
+  //TODO
 }
 
-void print_keys(char ** keys, int size){
+void print_keys(char keys[][], int size){
+  printf("Hello\n");
+  printf("%s\n", keys[0]);
+  printf("Hello\n");
   for(int a=0;a<size; a++){
-    printf("%s\n", *(keys)[size]);
+    printf("Here morocho\n");
+    printf("%s\n", keys[size]);
+    printf("Here morocho\n");
   }
+}
+
+void handle_error(int status){
+  if(status==1){
+    printf("Please supply the text file as a command line argument\n");
+  }else if(status==2){
+    printf("Cannot open the file %s\nExiting \n");
+  }
+  exit(0);
 }
 
 int main(int argc, char **argv){
-  if(argc < 2){ //Check the number of arguments
-    printf("Please supply the text file as a command line argument\n");
-    exit(0);
-  }
+  //Open the file
+  if(argc < 2) handle_error(1);
   FILE *fptr;
   fptr=fopen(argv[1],"r");
-  if(fptr==NULL){ //Check if the file was opened
-    printf("Cannot open the file %s\nExiting \n", argv[1]);
-    exit(0);
-  }
+  if(fptr==NULL) handle_error(2);
 
-  int size=lines(fptr);
+  //Data
+  int keys_size=linecount(fptr);
   int buff_index=0;
   int buff_size=4;
   int keys_index=0;
   char buff[buff_size];
   char c;
-  char keys[size][buff_size];
+  char keys[keys_size][buff_size];
+
+  //printf("Size of keys %d\n", sizeof(keys));
   memset(buff,0,buff_size);
   while((c=fgetc(fptr))!=EOF){
     if(c!=EOF && c!='\n'){
@@ -58,17 +72,16 @@ int main(int argc, char **argv){
       buff_index++;
     }else{
       buff[buff_index] = '\0';
-      strncpy(keys[keys_index], buff,3);
+      strcpy(keys[keys_index], buff);
       printf("copying one key: %s\n",buff);
+      printf("copied value :%s\n", keys[keys_index]);
       memset(buff,0,buff_size);
       buff_index=0;
       keys_index++;
     }
   }
-
-  for(int a=0;a<size;a++){
-    printf("-> %s\n", keys[a]);
-  }
   fclose(fptr);
+  print_keys(keys, keys_size);
   return 0;
 }
+
